beac15accfd14c1d53814beae2574ae34f4b15e0
huyquy281220 <huyquy2812@gmail.com>
2025-06-01 18:04:01 +0700

    install and config multer library

---
+++ backend/src/config/multer.config.ts
@@ -0,0 +1,23 @@
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { v4 as uuidv4 } from 'uuid';
+
+export const multerOptions = {
+  storage: diskStorage({
+    destination: './uploads/avatars',
+    filename: (req, file, cb) => {
+      const uniqueSuffix = `${uuidv4()}${extname(file.originalname)}`;
+      cb(null, uniqueSuffix);
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
+      cb(null, true);
+    } else {
+      cb(new Error('Unsupported file type'), false);
+    }
+  },
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+  },
+};
