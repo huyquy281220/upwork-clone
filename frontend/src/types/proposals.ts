0e837e14c54a60f456bf48bfc68974c37819a7ca
huyquy281220 <huyquy2812@gmail.com>
2025-06-30 22:11:52 +0700

    Add ProposalProps interface to proposals.ts for enhanced proposal management
    - Introduced the ProposalProps interface to define the structure of proposal objects, including properties such as id, freelancerId, jobId, coverLetter, pricing, timeline, attachment, status, createdAt, updatedAt, and nested job and freelancer details.
    - This addition improves type safety and clarity in managing proposal data throughout the application.

--- frontend/src/types/proposals.ts
+++ frontend/src/types/proposals.ts
@@ -6,3 +6,36 @@ export interface CreateProposalProps {
   timeline: string;
   attachment: File | null;
 }
+
+export interface ProposalProps {
+  id: string;
+  freelancerId: string;
+  jobId: string;
+  coverLetter: string;
+  pricing: number;
+  timeline: string;
+  attachment: string;
+  status: "PENDING" | "ACCEPTED" | "REJECTED";
+  createdAt: string;
+  updatedAt: string;
+  job: {
+    title: string;
+    description: string;
+    hourlyRateMin: number | null;
+    hourlyRateMax: number | null;
+    fixedPrice: number | null;
+  };
+  freelancer: {
+    id: string;
+    userId: string;
+    hourlyRate: number;
+    title: string;
+    overview: string | null;
+    available: "LESS_THAN_30" | "MORE_THAN_30" | "FULL_TIME";
+    user: {
+      fullName: string;
+      address: string;
+      verified: boolean;
+    };
+  };
+}
