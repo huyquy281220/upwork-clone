1cdc1e28e6b6fd697d2b9237f6ab09ebf7be21a6
huyquy281220 <huyquy2812@gmail.com>
2025-07-17 22:34:24 +0700

    Refactor NotFound component to simplify navigation and enhance user experience
    - Removed session handling and redirection logic from the `NotFound` component, streamlining the code.
    - Replaced the link with a button that allows users to navigate back to the previous page, improving usability.
    - This change focuses on providing a clearer and more straightforward user interaction when a resource is not found.

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,31 +1,16 @@
 "use client";
 
-import { InfiniteLoading } from "@/components/common/InfiniteLoading";
-import { Role } from "@/types/user";
-import { useSession } from "next-auth/react";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function NotFound() {
-  const { data: session, status } = useSession();
-  const [redirectUrl, setRedirectUrl] = useState("/client");
-
-  useEffect(() => {
-    if (!session) return;
-    if (session?.user.role === Role.FREELANCER) {
-      setRedirectUrl("/freelancer/find-work");
-    } else {
-      setRedirectUrl("/client/dashboard");
-    }
-  }, [session]);
-
-  if (status === "loading") return <InfiniteLoading />;
+  const router = useRouter();
 
   return (
     <div>
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
-      <Link href={redirectUrl}>Return Home</Link>
+      <Button onClick={() => router.back()}>Go back</Button>
     </div>
   );
 }
