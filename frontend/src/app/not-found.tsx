eec657f5e17151cadf7030ca44421a7ea2834172
huyquy281220 <huyquy2812@gmail.com>
2025-07-17 22:54:02 +0700

    Update NotFound component to restore navigation functionality
    - Reintroduced the `useRouter` hook in the `NotFound` component to enable navigation back to the previous page.
    - Updated the button to utilize the router's back method, enhancing user experience by allowing users to easily return to their last location.
    - This change aims to improve usability and maintain a straightforward interaction when a resource is not found.

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function NotFound() {
+  const router = useRouter();
+
   return (
     <div>
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
-      <Button>Go back</Button>
+      <Button onClick={() => router.back()}>Go back</Button>
     </div>
   );
 }
