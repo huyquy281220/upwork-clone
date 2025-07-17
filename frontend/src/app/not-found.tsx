11b96bcfc7d74d52a16ada10133ee160a416f43b
huyquy281220 <huyquy2812@gmail.com>
2025-07-17 22:37:12 +0700

    Refactor NotFound component to remove router dependency and simplify navigation
    - Eliminated the use of the `useRouter` hook in the `NotFound` component, streamlining the code.
    - Updated the button to remove the navigation logic, focusing on a cleaner user interface.
    - This change enhances the component's simplicity and maintains a straightforward user experience when a resource is not found.

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,16 +1,11 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
 
 export default function NotFound() {
-  const router = useRouter();
-
   return (
     <div>
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
-      <Button onClick={() => router.back()}>Go back</Button>
+      <Button>Go back</Button>
     </div>
   );
 }
