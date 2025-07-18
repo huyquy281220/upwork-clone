bba0ff0220651ecaca7f5c231dd928dcdedefd4b
huyquy281220 <huyquy2812@gmail.com>
2025-07-18 11:26:08 +0700

    Refactor MyProposalsPage to utilize MyProposals component
    - Replaced the existing implementation of the `ProposalsPage` component with a direct return of the `MyProposals` component, significantly simplifying the code structure.
    - Removed unnecessary state management, imports, and logic related to proposals, enhancing maintainability and clarity.
    - This change focuses on a more modular approach, improving the organization of the codebase for managing freelancer proposals functionality.

--- frontend/src/pages-section/freelancer/Proposals/my-proposals/index.ts
+++ frontend/src/pages-section/freelancer/Proposals/my-proposals/index.ts
@@ -3,8 +3,10 @@ import { ProposalsFilters } from "./Filter";
 import { ProposalsEmptyState } from "./EmptyState";
 import { ProposalsList } from "./List";
 import { ProposalsSearchSort } from "./Search";
+import { MyProposals } from "./MyProposals";
 
 export {
+  MyProposals,
   ProposalsEmptyState,
   ProposalsFilters,
   ProposalsHeader,
