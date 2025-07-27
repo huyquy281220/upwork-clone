2a61b09c8a0ecd8efd2b05fab4c0c4f83eda3bb3
huyquy281220 <huyquy2812@gmail.com>
2025-07-27 23:06:30 +0700

    Enhance SessionWrapper to prevent refetching on window focus
    - Updated the SessionWrapper component to include the `refetchOnWindowFocus` prop set to false in the SessionProvider, improving performance by preventing unnecessary data refetching when the window regains focus.

--- frontend/src/providers/SessionWrapper.tsx
+++ frontend/src/providers/SessionWrapper.tsx
@@ -4,5 +4,7 @@ import { ChildrenProps } from "@/types";
 import { SessionProvider } from "next-auth/react";
 
 export default function SessionWrapper({ children }: ChildrenProps) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider refetchOnWindowFocus={false}>{children}</SessionProvider>
+  );
 }
