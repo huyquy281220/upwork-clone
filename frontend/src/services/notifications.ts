71b5efc70f5cc0b20f5072672535d8220f9847a9
huyquy281220 <huyquy2812@gmail.com>
2025-07-28 23:06:16 +0700

    Update notification fetching to include pagination parameters
    - Modified the `getNotificationsByUserId` function to append `&limit=5&page=1` to the API call, enhancing the retrieval of notifications by limiting the results and specifying the page number.
    - This change aligns with recent pagination updates across the codebase, ensuring consistency in data fetching practices.

--- frontend/src/services/notifications.ts
+++ frontend/src/services/notifications.ts
@@ -3,7 +3,9 @@ import api from "./api";
 const apiURL = process.env.NEXT_PUBLIC_API_URL;
 
 export const getNotificationsByUserId = async (userId: string) => {
-  const response = await api.get(`${apiURL}/notifications?userId=${userId}`);
+  const response = await api.get(
+    `${apiURL}/notifications?userId=${userId}&limit=5&page=1`
+  );
 
   return response.data;
 };
