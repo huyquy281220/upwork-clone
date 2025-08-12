5fe75f932d84d2f52623305f7fca6075fa7ccb71
huyquy281220 <huyquy2812@gmail.com>
2025-08-12 19:59:29 +0700

    Refactor import paths for Apply and MyProposals components to align with updated directory structure
    - Updated the import statements for the `CreateProposal` and `MyProposals` components in their respective `page.tsx` files to reflect their new paths within the `proposals` directory.
    - This change enhances consistency in import practices and contributes to a more organized project structure, following recent updates to module resolution strategies.

--- frontend/src/app/freelancer/jobs/[jobId]/apply/page.tsx
+++ frontend/src/app/freelancer/jobs/[jobId]/apply/page.tsx
@@ -1,6 +1,6 @@
 export const dynamic = "force-dynamic";
 
-import { CreateProposal } from "@/pages-section/freelancer/Proposals";
+import { CreateProposal } from "@/pages-section/freelancer/proposals";
 
 export default function Apply() {
   return <CreateProposal />;
