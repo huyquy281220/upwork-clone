6e0b06440f4bdc24b28aa444486c369f82d2a419
huyquy281220 <huyquy2812@gmail.com>
2025-08-28 21:55:07 +0700

    Refactor timeline selection options in ContractTerms and Timeline components for consistency
    - Updated the SelectItem values in both ContractTerms and Timeline components to use consistent formatting by replacing hyphens with spaces in the duration options.
    - Ensured uniformity in the user interface for better readability and user experience.

--- frontend/src/pages-section/freelancer/proposals/create-proposal/Timeline.tsx
+++ frontend/src/pages-section/freelancer/proposals/create-proposal/Timeline.tsx
@@ -32,12 +32,12 @@ export function TimelineSection({
             </SelectTrigger>
             <SelectContent>
               <SelectItem value="less-than-1-week">Less than 1 week</SelectItem>
-              <SelectItem value="1-2-weeks">1-2 weeks</SelectItem>
-              <SelectItem value="2-4-weeks">2-4 weeks</SelectItem>
-              <SelectItem value="1-2-months">1-2 months</SelectItem>
-              <SelectItem value="2-3-months">2-3 months</SelectItem>
-              <SelectItem value="3-6-months">3-6 months</SelectItem>
-              <SelectItem value="more-than-6-months">
+              <SelectItem value="1-2 weeks">1-2 weeks</SelectItem>
+              <SelectItem value="2-4 weeks">2-4 weeks</SelectItem>
+              <SelectItem value="1-2 months">1-2 months</SelectItem>
+              <SelectItem value="2-3 months">2-3 months</SelectItem>
+              <SelectItem value="3-6 months">3-6 months</SelectItem>
+              <SelectItem value="more than 6 months">
                 More than 6 months
               </SelectItem>
             </SelectContent>
