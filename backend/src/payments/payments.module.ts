17d5e4fe3517e6a1b3ab39f7732b0a5f5406e753
huyquy281220 <huyquy2812@gmail.com>
2025-09-06 17:50:59 +0700

    Integrate SocketModule into PaymentsModule for real-time payment updates

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -3,10 +3,11 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { StripeModule } from 'src/stripe/stripe.module';
+import { SocketModule } from 'src/socket/socket.module';
 
 @Module({
   controllers: [PaymentsController],
-  imports: [PrismaModule, forwardRef(() => StripeModule)],
+  imports: [PrismaModule, forwardRef(() => StripeModule), SocketModule],
   providers: [PaymentsService],
   exports: [PaymentsService],
 })
